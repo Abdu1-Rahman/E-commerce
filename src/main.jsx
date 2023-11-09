2cb959e11992a7c4891571b3100f9fdcc2a82bf9
Nawaf v p <nawafvp.dev@gmail.com>
2023-11-09 14:54:43 +0530

    Fix : style overlap issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter,RouterProvider} from "react-router-dom";
 import Cart from './pages/Cart.jsx';
 import LoginSignup from './pages/LoginSignup.jsx';
